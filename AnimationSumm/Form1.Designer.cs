028369ea82b0be8689fa222f905b28d2c154f4c2
tyler gerber <tyler gerber>
2017-09-21 12:36:26 -0400

    Add project files.

---
+++ AnimationSumm/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace AnimationSumm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.name.Cursor = System.Windows.Forms.Cursors.Default;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.ForeColor = System.Drawing.Color.Black;
+            this.name.Location = new System.Drawing.Point(24, 316);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(189, 29);
+            this.name.TabIndex = 1;
+            this.name.Text = "By: Tyler Gerber";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AnimationSumm.Properties.Resources._0af7aa43cbc131d922f6fcfe17e1d290;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(497, 392);
+            this.Controls.Add(this.name);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label name;
+    }
+}
+
---
+++ AnimationSumm/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AnimationSumm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //Start Up clean screen
+            BackgroundImage = null;
+            name.Dispose();
+            Refresh();
+            
+            //Frame 1
+            Graphics formGraphics = this.CreateGraphics();
+            Pen blackPen  = new Pen(Color.Black, 2);
+            SolidBrush grayFill = new SolidBrush(Color.Gray);
+            SolidBrush blackFill = new SolidBrush(Color.Black);
+            SolidBrush head = new SolidBrush(Color.Beige);
+            SolidBrush bod = new SolidBrush(Color.Red);
+            SolidBrush brown = new SolidBrush(Color.Chocolate);
+            SolidBrush pelvisAndLeg = new SolidBrush(Color.Blue);
+            formGraphics.FillRectangle(grayFill, 0, 0, 600, 600);
+
+            //ball
+            formGraphics.FillEllipse(blackFill, -35, 25, 25, 35);
+
+
+            //dude
+            formGraphics.FillEllipse(brown, 258, 185, 58, 70);
+            formGraphics.FillEllipse(bod, 257, 225, 55, 140);
+            formGraphics.FillEllipse(pelvisAndLeg, 258, 335, 55, 30);
+            formGraphics.FillEllipse(pelvisAndLeg, 257, 350, 20, 50);
+            formGraphics.FillEllipse(pelvisAndLeg, 290, 350, 20, 50);
+            formGraphics.FillEllipse(head, 257, 200, 55, 75);
+            formGraphics.DrawArc(blackPen, 265, 240, 30, 20, 10, 130);
+            formGraphics.FillEllipse(blackFill, 265, 225, 5, 5);
+            formGraphics.FillEllipse(blackFill, 285, 225, 5, 5);
+
+            Thread.Sleep(100);
+
+            //ball
+            formGraphics.FillEllipse(blackFill, 40, 25, 25, 25);
+
+            //dude
+            formGraphics.FillEllipse(brown, 258, 185, 58, 70);
+            formGraphics.FillEllipse(bod, 257, 225, 55, 140);
+            formGraphics.FillEllipse(pelvisAndLeg, 258, 335, 55, 30);
+            formGraphics.FillEllipse(pelvisAndLeg, 257, 350, 20, 50);
+            formGraphics.FillEllipse(pelvisAndLeg, 290, 350, 20, 50);
+            formGraphics.FillEllipse(head, 257, 200, 55, 75);
+            formGraphics.DrawArc(blackPen, 265, 240, 30, 20, 10, 130);
+            formGraphics.FillEllipse(blackFill, 265, 225, 5, 5);
+            formGraphics.FillEllipse(blackFill, 285, 225, 5, 5);
+
+
+        }
+    }
+}
